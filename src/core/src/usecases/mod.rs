8e1a12d261338f9564ef17da1dc273a48bb00498
GuiNinov <60795300+GuiNinov@users.noreply.github.com>
2026-03-21 13:35:22 -0300

    Revert "Implement core package with repositories, migrations, and structured usecases"

--- src/core/src/usecases/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod use_case;
-pub mod task;
-pub mod graph;
-
-pub use use_case::UseCase;
-pub use task::*;
-pub use graph::*;
--- src/core/src/usecases/use_case.rs
+++
@@ -1,7 +0,0 @@
-use async_trait::async_trait;
-use luce_shared::LuceError;
-
-#[async_trait]
-pub trait UseCase<Input, Output> {
-    async fn execute(&self, input: Input) -> Result<Output, LuceError>;
-}
